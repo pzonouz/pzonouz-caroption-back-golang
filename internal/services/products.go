48e8ca34906cdb498b6c61bbf4fd967403f14079
peyman khalili zonouz <p.zonouz@gmail.com>
2025-11-11 07:49:46 +0330

    make generated show true

--- internal/services/products.go
+++ internal/services/products.go
@@ -115,6 +115,7 @@ func (s *Service) GenerateProducts() ([]Product, error) {
 		slug = EXCLUDED.slug,
 		keywords = EXCLUDED.keywords,
 		image_id = EXCLUDED.image_id
+		show = EXCLUDED.show
 	RETURNING id;
 	`
 
